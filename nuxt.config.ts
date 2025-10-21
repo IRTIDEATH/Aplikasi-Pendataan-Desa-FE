e34465f4e52a64ed3b15cfa7221e28ff82d3ad58
IRTIDEATH <enderjhass@gmail.com>
2025-10-21 19:35:55 +0700

    Membuat setup eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,23 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@vueuse/nuxt', '@pinia/nuxt'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+    '@nuxt/eslint',
+  ],
+  eslint: {
+    config: {
+      stylistic: {
+        semi: true,
+        quotes: 'double',
+        commaDangle: 'always-multiline',
+        indent: 'tab'
+      }
+    }
+  },
   shadcn: {
     /**
      * Prefix for all the imported component
