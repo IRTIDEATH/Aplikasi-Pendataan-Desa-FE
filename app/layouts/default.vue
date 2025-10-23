a0f6ab15bd6bff2decc83214149d31b95a832d2a
IRTIDEATH <enderjhass@gmail.com>
2025-10-23 14:22:11 +0700

    Membuat Antarmuka Pengguna pada halaman utama

---
+++ app/layouts/default.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import NavigationHeader from "~/components/layouts/app-layout/NavigationHeader.vue";
+</script>
+
+<template>
+	<div>
+		<NavigationHeader />
+		<slot />
+	</div>
+</template>
